f501dbc2fa1a1e4f30b64862996ecc6967bcebe1
Elisha Tam <elisha.tam@gmail.com>
2018-04-20 13:24:09 -0700

    Add in data with working python

--- 20180418/EVT2_1_Thermals2.py
+++ 20180418/EVT2_1_Thermals2.py
@@ -8,12 +8,13 @@ Created on Wed Apr 18 16:29:20 2018
 import matplotlib.dates as mtdates
 import matplotlib.pyplot as plt
 import numpy as np
+from datetime import datetime
 
 
 
 # reading all lines into list of string
-with open(r"ZoneDungeon3D255.csv") as f:
-    content = f.readlines()
+with open(r"20180418_zone2D_0.csv") as f:
+    content = f.readlines()[1:]
 
 content = [x.strip() for x in content]
 
@@ -21,9 +22,12 @@ dates = []
 zone9_list = []
 for line in content:
     splt = line.split(',')
-    dates.append(splt[3])
-    zone9_list.append(float(splt[8]))
-    
+    #dates.append(splt[3])
+    #zone9_list.append(float(splt[8]))
+    #dates.append(splt[0])
+    dates.append(datetime.strptime(splt[0], '%H:%M:%S').time())
+    zone9_list.append(float(splt[2]))
+
 #time = np.arange(0, len(dates))
 plt.figure()
 #plt.scatter(time, zone9_list, label="Zone9-lcd_therm")
@@ -31,8 +35,8 @@ plt.scatter(dates, zone9_list, label="Zone9-lcd_therm", color="y")
 
 
 # reading all lines into list of string
-with open(r"FlirDungeon3D255_minute.csv") as f:
-    content = f.readlines()
+with open(r"20180418_flir2D_0.csv") as f:
+    content = f.readlines()[13:]
 
 content = [x.strip() for x in content]
 
@@ -43,11 +47,12 @@ point3 = []
 point4 = []
 for line in content:
     splt = line.split(',')
-    dates2.append(float(splt[5]))
-    point1.append(float(splt[6]))
-    point2.append(float(splt[7]))
-    point3.append(float(splt[8]))
-    point4.append(float(splt[9]))
+    #dates2.append(float(splt[2]))
+    dates2.append(datetime.strptime(splt[2], '%H:%M:%S').time())
+    point1.append(float(splt[3])) 
+    point2.append(float(splt[4])) 
+    point3.append(float(splt[5])) 
+    point4.append(float(splt[6])) 
 
 #plt.scatter(dates2,point1)
 
@@ -68,24 +73,25 @@ plt.scatter(dates2desired,point4desired,label="Spot4",color="k")
 #plt.xlim(0,30)
 #plt.ylim(0,55)
 
-plt.title('Dungeon 3D:255')
-plt.xlabel('Time (min)')
+plt.title('2D:0/255')
+plt.xlabel('Time')
 plt.ylabel('Temperature (C)')
 #plt.xlim(-5,30)
 plt.xticks(rotation=30)
-plt.ylim(0,55)
+#plt.ylim(0,55)
+plt.ylim(0,(max(zone9_list)+10))
 plt.legend(loc='lower right', frameon=True)
 
 
 
-plt.annotate('%0.2f' % zone9_list[-1], xy=(1,zone9_list[-1]), xytext=(0, 5), 
+plt.annotate('%0.2f' % zone9_list[-1], xy=(1,zone9_list[-1]), xytext=(0, -20), 
                  xycoords=('axes fraction', 'data'), textcoords='offset points',color="y")
 plt.annotate('%0.2f' % point1desired[-1], xy=(1,point1desired[-1]), xytext=(0, 10), 
                  xycoords=('axes fraction', 'data'), textcoords='offset points',color="b")
-plt.annotate('%0.2f' % point2desired[-1], xy=(1,point2desired[-1]), xytext=(0, -2), 
+plt.annotate('%0.2f' % point2desired[-1], xy=(1,point2desired[-1]), xytext=(0, 10), 
                  xycoords=('axes fraction', 'data'), textcoords='offset points',color="r")
-plt.annotate('%0.2f' % point3desired[-1], xy=(1,point3desired[-1]), xytext=(0, -10), 
+plt.annotate('%0.2f' % point3desired[-1], xy=(1,point3desired[-1]), xytext=(0, 0), 
                  xycoords=('axes fraction', 'data'), textcoords='offset points',color="g")
-plt.annotate('%0.2f' % point4desired[-1], xy=(1,point4desired[-1]), xytext=(0, 10), 
+plt.annotate('%0.2f' % point4desired[-1], xy=(1,point4desired[-1]), xytext=(0, -10), 
                  xycoords=('axes fraction', 'data'), textcoords='offset points',color="k")
 plt.show()
