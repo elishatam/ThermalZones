1c5c1883dc5ef28a896ed8d89f068496e1027ad6
Elisha Tam <elisha.tam@gmail.com>
2018-04-23 14:46:46 -0700

    Modify time formatting so don't need to adjust formatting in Google sheets

--- 20180423/EVT2_1_Thermals2.py
+++ 20180423/EVT2_1_Thermals2.py
@@ -5,15 +5,17 @@ Created on Wed Apr 18 16:29:20 2018
 
 @author: elishatam
 """
-import matplotlib.dates as mtdates
+#import matplotlib.dates as mtdates
 import matplotlib.pyplot as plt
-import numpy as np
+#import numpy as np
 from datetime import datetime
 
 #Change these variables
-filename_zone = "20180418_zone2D_0.csv"    
-filename_flir = "20180420Flir-Sleep_noUSB.csv"
-title = '2D:0/255'
+filename_zone = "04_Sleep_USBin_NotCharging_Front.csv"    
+filename_flir = "20180423Flir - 04_Sleep_USBin_Notcharging.csv"
+title = 'Sleep, USB In, Not Charging, Front'
+
+plt.figure()
 
 # reading all lines into list of string
 with open(filename_zone, 'r') as f:
@@ -26,13 +28,15 @@ dates = []
 zone9_list = []
 for line in content:
     splt = line.split(',')
-    dates.append(datetime.strptime(splt[0], '%H:%M:%S').time())
-    zone9_list.append(float(splt[2]))
+    dates.append(datetime.strptime(splt[1], '%H:%M:%S').time())
+    zone9_list.append(float(splt[7]))
 
 #time = np.arange(0, len(dates))
-plt.figure()
+
 #plt.scatter(time, zone9_list, label="Zone9-lcd_therm")
 plt.scatter(dates, zone9_list, label="Zone9-lcd_therm", color="y")
+plt.annotate('%0.2f' % zone9_list[-1], xy=(1,zone9_list[-1]), xytext=(0, 0), 
+                 xycoords=('axes fraction', 'data'), textcoords='offset points',color="y")
 
 
 # reading all lines into list of string
@@ -47,45 +51,60 @@ point1 = []
 point2 = []
 point3 = []
 point4 = []
+point5 = []
+point6 = []
 for line in content:
     splt = line.split(',')
-    dates2.append(datetime.strptime(splt[2], '%H:%M:%S').time())
-    point1.append(float(splt[3])) 
-    point2.append(float(splt[4])) 
-    point3.append(float(splt[5])) 
-    point4.append(float(splt[6])) 
+    #dates2.append(datetime.strptime(splt[2], '%H:%M:%S').time()) 
+    dates2.append(datetime.strptime(splt[1], '%I:%M:%S %p').time())
+    point1.append(float(splt[4])) 
+    point2.append(float(splt[5])) 
+    point3.append(float(splt[6])) 
+    point4.append(float(splt[7])) 
+    point5.append(float(splt[8])) 
+    point6.append(float(splt[9])) 
 
-
+#Pick every 200 points
 dates2desired = dates2[1::200]
 point1desired = point1[1::200]
 point2desired = point2[1::200]
 point3desired = point3[1::200]
 point4desired = point4[1::200]
+point5desired = point5[1::200]
+point6desired = point6[1::200]
+
+#print(dates2desired)
 
 plt.scatter(dates2desired,point1desired,label="Spot1",color="b")
 plt.scatter(dates2desired,point2desired,label="Spot2",color="r")
 plt.scatter(dates2desired,point3desired,label="Spot3",color="g")
 plt.scatter(dates2desired,point4desired,label="Spot4",color="k")
-
+plt.scatter(dates2desired,point5desired,label="Spot5",color="c")
+plt.scatter(dates2desired,point5desired,label="Spot6",color="m")
 
 plt.title(title)
 plt.xlabel('Time')
 plt.ylabel('Temperature (C)')
 #plt.xlim(-5,30)
 plt.xticks(rotation=30)
-plt.ylim(0,(max(zone9_list)+10))
+#plt.ylim(0,(max(zone9_list)+10))
+plt.ylim(0,(max(point1)+10))
+print(max(point1))
 plt.legend(loc='lower right', frameon=True)
 
 
 
-plt.annotate('%0.2f' % zone9_list[-1], xy=(1,zone9_list[-1]), xytext=(0, -20), 
-                 xycoords=('axes fraction', 'data'), textcoords='offset points',color="y")
-plt.annotate('%0.2f' % point1desired[-1], xy=(1,point1desired[-1]), xytext=(0, 10), 
+plt.annotate('%0.2f' % point1desired[-1], xy=(1,point1desired[-1]), xytext=(0, 16), 
                  xycoords=('axes fraction', 'data'), textcoords='offset points',color="b")
-plt.annotate('%0.2f' % point2desired[-1], xy=(1,point2desired[-1]), xytext=(0, 10), 
+plt.annotate('%0.2f' % point2desired[-1], xy=(1,point2desired[-1]), xytext=(0, 18), 
                  xycoords=('axes fraction', 'data'), textcoords='offset points',color="r")
 plt.annotate('%0.2f' % point3desired[-1], xy=(1,point3desired[-1]), xytext=(0, 0), 
                  xycoords=('axes fraction', 'data'), textcoords='offset points',color="g")
-plt.annotate('%0.2f' % point4desired[-1], xy=(1,point4desired[-1]), xytext=(0, -10), 
+plt.annotate('%0.2f' % point4desired[-1], xy=(1,point4desired[-1]), xytext=(0, -20), 
                  xycoords=('axes fraction', 'data'), textcoords='offset points',color="k")
+plt.annotate('%0.2f' % point5desired[-1], xy=(1,point5desired[-1]), xytext=(0, -20), 
+                 xycoords=('axes fraction', 'data'), textcoords='offset points',color="c")
+plt.annotate('%0.2f' % point6desired[-1], xy=(1,point5desired[-1]), xytext=(0, -30), 
+                 xycoords=('axes fraction', 'data'), textcoords='offset points',color="m")
 plt.show()
+
